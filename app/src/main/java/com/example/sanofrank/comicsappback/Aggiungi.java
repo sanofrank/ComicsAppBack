a95eed2e5523e71821fb4883c43cc98281f299b4
sanofrank <sanofrank@gmail.com>
2015-10-05 18:15:55 +0200

    Aggiunte

--- app/src/main/java/com/example/sanofrank/comicsappback/Aggiungi.java
+++ app/src/main/java/com/example/sanofrank/comicsappback/Aggiungi.java
@@ -1,9 +1,12 @@
 package com.example.sanofrank.comicsappback;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Aggiungi extends ActionBarActivity {
@@ -12,6 +15,16 @@ public class Aggiungi extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aggiungi);
+        getSupportActionBar().hide();
+
+        Button btn = (Button) findViewById(R.id.aggiungi);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent add = new Intent(Aggiungi.this, SimpleScannerActivity.class);
+                startActivity(add);
+            }
+        });
     }
 
     @Override
