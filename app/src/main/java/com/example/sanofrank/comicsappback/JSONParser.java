a7994e37733a48d899689c6106ff6d27d8c15c3a
Eugenio Paluello <eugeniopaluello@MacBook-Pro-di-Eugenio.local>
2016-04-18 19:35:50 +0200

    +prova push project

--- app/src/main/java/com/example/sanofrank/comicsappback/JSONParser.java
+++ app/src/main/java/com/example/sanofrank/comicsappback/JSONParser.java
@@ -4,26 +4,26 @@ package com.example.sanofrank.comicsappback;
  * Created by Sanofrank on 11/11/15.
  */
 
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStream;
-        import java.io.InputStreamReader;
-        import java.io.UnsupportedEncodingException;
-        import java.util.List;
-
-        import org.apache.http.HttpEntity;
-        import org.apache.http.HttpResponse;
-        import org.apache.http.NameValuePair;
-        import org.apache.http.client.ClientProtocolException;
-        import org.apache.http.client.entity.UrlEncodedFormEntity;
-        import org.apache.http.client.methods.HttpGet;
-        import org.apache.http.client.methods.HttpPost;
-        import org.apache.http.client.utils.URLEncodedUtils;
-        import org.apache.http.impl.client.DefaultHttpClient;
-        import org.json.JSONException;
-        import org.json.JSONObject;
-
         import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public class JSONParser {
 
@@ -41,6 +41,8 @@ public class JSONParser {
     public JSONObject makeHttpRequest(String url, String method,
                                       List<NameValuePair> params) {
 
+
+        //prova push
         // Making HTTP request
         try {
 
